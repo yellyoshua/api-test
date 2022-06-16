9e80bb78298d072ebe4a23ef48f20e9f95ca6151
yellyoshua <usuario1529yoshua@hotmail.com>
2022-06-16 17:24:57 -0500

    add total_routes spec

--- server/modules/totals/specs/total_routes.spec.js
+++ server/modules/totals/specs/total_routes.spec.js
@@ -1,7 +1,27 @@
+import database from '../../utils_for_specs/database.js';
+import request from '../../utils_for_specs/request.js';
+import total_routes from '../total_routes.js';
+
 describe('Total routes', () => {
+  beforeEach(async () => {
+    await database.clearAndLoad([
+      `${__dirname}/fixtures/users.js`,
+      `${__dirname}/fixtures/students.js`
+    ]);
+  });
+
+  afterEach(async () => {
+    await database.close();
+    jest.clearAllMocks();
+  });
+
   describe('GET /totals', () => {
-    it('should return the total number of users and students', () => {
-      expect(1).toBe(1);
+    it('should return the total number of users and students', async () => {
+      const data = await request(total_routes).get();
+
+      expect(data.status).toBe(200);
+      expect(data.errors).toEqual([]);
+      expect(data.response.total).toBe(6);
     });
   });
 });
