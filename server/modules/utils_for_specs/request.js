cdd15f3dbbe35380d63a1c2049e03a530b0b0b38
yellyoshua <usuario1529yoshua@hotmail.com>
2022-06-17 12:19:03 -0500

    add request utils for testing routes

--- server/modules/utils_for_specs/request.js
+++ server/modules/utils_for_specs/request.js
@@ -1,30 +1,61 @@
 export default (routes) => {
-  const resolveRequest = (request) => {
-    const mockResponse = {
-      json: jest.fn().mockReturnThis(),
-      status: jest.fn().mockReturnThis()
+  const resolveRequest = (method, request_data) => {
+    const appMock = {
+      get: jest.fn(),
+      post: jest.fn(),
+      put: jest.fn(),
+      delete: jest.fn() 
     };
 
+    routes(appMock);
+    
     const resolveHandler = async (path, handler) => {
-      await handler(request, mockResponse);
-      const status = mockResponse.status.mock.calls[0][0];
-      const {response, errors} = mockResponse.json.mock.calls[0][0];
+      const responseHandlerMock = {
+        json: jest.fn().mockReturnThis(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      await handler(request_data, responseHandlerMock);
+      const {response, errors} = responseHandlerMock.json.mock.calls[0][0];
+      const status = responseHandlerMock.status.mock.calls[0][0];
+
+      responseHandlerMock.json.mockClear();
+      responseHandlerMock.status.mockClear();
       
       return {response, errors, status};
     };
-
+    
     return new Promise((resolve) => {
-      routes({
-        get (path, handler) {
-          resolve(resolveHandler(path, handler));
-        }
-      });
+      const [path, handler] = appMock[method].mock.calls[0];
+      appMock[method].mockClear();
+      resolve(resolveHandler(path, handler));
     });
   };
 
   return {
     async get (req_query = {}) {
-      const response = await resolveRequest({
+      const response = await resolveRequest('get', {
+        query: req_query
+      });
+
+      return response;
+    },
+    async post (req_body = {}) {
+      const response = await resolveRequest('post', {
+        body: req_body
+      });
+
+      return response;
+    },
+    async put (req_body = {}) {
+      const response = await resolveRequest('put', {
+        body: req_body
+      });
+
+      return response;
+    },
+    async delete (req_query = {}) {
+      const response = await resolveRequest('delete', {
         query: req_query
       });
 
